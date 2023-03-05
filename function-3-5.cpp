16358194e9219864188ab56640588cb03e0ecd02
Hard Patel <hardpatel@Hards-MacBook-Pro.local>
2023-03-05 20:08:49 +1030

    g

--- function-3-5.cpp
+++ function-3-5.cpp
@@ -9,10 +9,10 @@ double sum_even(double array[], int n){
     //declare a variable for sum to 0
     int sum=0;
 
-    //loop through array 
-    for(int i=0; i<n; i++){
+    //loop through array but only iterate up by 2 so you always land on even position elemet 
+    for(int i=0; i<n; i+=2){
         //only add the value of the element to sum if it is in an even position
-        sum+=array[i%2==0];
+        sum+=array[i];
     
     }
 
