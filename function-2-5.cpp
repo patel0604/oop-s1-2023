57d86b6510ba1fb8f33d536dec1f63638cc84b3c
Hard Patel <hardpatel@Hards-MacBook-Pro.local>
2023-03-05 20:16:02 +1030

    ghf

--- function-2-5.cpp
+++ function-2-5.cpp
@@ -7,9 +7,9 @@ bool is_descending(int array[], int n){
     }
     
     //loop through the array
-    for(int i=0;i<n;i++){
+    for(int i=1;i<n;i++){
         //if current element is less than the next element, then the array is not descending. so return false  
-        if (array[i]>array[i+1]){
+        if (array[i]>array[i-1]){
             return false;
         }
     }
