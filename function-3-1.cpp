a78d666f47937db2ecef02e9e592e453e09260a1
Hard Patel <hardpatel@Hards-MacBook-Pro.local>
2023-03-05 20:18:56 +1030

    fhe

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -10,9 +10,8 @@ bool is_fanarray(int array[], int n){
     for(int i=0; i<n/2; i++){
 
         //check if the current element is not equal to the correct palindrom position. ie if n =5 and array[0] != array[4], then return false
-        if(array[i] != array[n-i-1]){
+        if(array[i] != array[n-i-1] || array[i]>array[i+1]){
             return false;
-            break;
         }
     }
 
