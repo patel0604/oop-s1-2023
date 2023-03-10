59fbdce26d1656257b36711d6a830d601a5fdeac
Hard Patel <hardpatel@Hards-MacBook-Pro.local>
2023-03-10 16:13:40 +1030

    f

--- main-1-4.cpp
+++ main-1-4.cpp
@@ -4,7 +4,7 @@ using namespace std;
 extern void print_scaled(int array[3][3],int scale);
 
 int main(){
-    int array[3][3] = {0};
+    int array[3][3] = {1,2,3,4,5,6,7,8,9};
     int scale = 3;
     print_scaled(array, scale);
 
