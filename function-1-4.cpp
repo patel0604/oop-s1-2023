e57083913ba50dc8084c3a7924ec0186cad18281
Hard Patel <hardpatel@Hards-MacBook-Pro.local>
2023-03-10 16:18:29 +1030

    g

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -3,7 +3,7 @@ using namespace std;
 void print_scaled(int array[3][3],int scale){
     for(int i=0; i<3; i++){
         for(int j=0; j<3; j++){
-            array[i][j] = array[i][j]*scale;
+            cout<< array[i][j]*scale<< " ";
         }
     }
 }
--- main-1-4.cpp
+++ main-1-4.cpp
@@ -7,12 +7,5 @@ int main(){
     int array[3][3] = {1,2,3,4,5,6,7,8,9};
     int scale = 3;
     print_scaled(array, scale);
-
-    for(int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
-            cout<<array[i][j]<<" ";
-        }
-        cout<<endl;
-    }
     return 0;
 }  
