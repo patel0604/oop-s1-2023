162c0f1147929d86eef99c419121c8b344ef232b
Hard Patel <hardpatel@Hards-MacBook-Pro.local>
2023-03-24 10:05:09 +1030

    h

--- main-1-4.cpp
+++ main-1-4.cpp
@@ -2,5 +2,21 @@
 #include "Person.h"
 
 extern PersonList shallowCopyPersonList(PersonList pl){
-    
+    int n = 4;
+    PersonList p_list;
+    p_list.numPeople = n;
+    p_list.people = new Person[n];
+
+    for(int i=0; i<n;i++){
+        p_list.people[i].name = "Jane Doe";
+        p_list.people[i].age = 1;
+    }
+
+    PersonList shallow_copy_list = shallowCopyPersonList(p_list);
+
+    for(int i=0; i<n;i++){
+
+        cout<<shallow_copy_list.people[i].name<<endl;
+        cout<<shallow_copy_list.people[i].age<<endl;
+    }
 }
