728d283855c87f4f4634890677bb28d91e53dd7f
Hard Patel <hardpatel@Hards-MacBook-Pro.local>
2023-04-09 18:17:45 +0930

    s

---
+++ ParkingLot.cpp
@@ -0,0 +1,42 @@
+#include "ParkingLot.h"
+#include "Vehicle.h"
+#include <iostream>
+using namespace std;
+
+ParkingLot::ParkingLot(): max_amount(0) {
+
+}
+
+ParkingLot::ParkingLot(int max_size): max_amount(max_amount) {
+  
+}
+
+int ParkingLot::getCount(){
+    return vehicles_counter;
+}
+
+void ParkingLot::parkVehicle(Vehicle* parking_space){
+    if (getCount()!=max_amount) {
+        vehicles_parked[vehicles_counter] = parking_space[0];
+        vehicles_counter++;
+    } 
+    else {
+        cout << "There are no empty spaces" << endl;
+    }
+}
+
+int ParkingLot::countOverstayingVehicles(int maxParkingDuration) {
+    int amount_overstay = 0;
+    for (int i = 0; i < vehicles_counter; i++){
+        if (vehicles_parked[i].getParkingDuration() > maxParkingDuration) {
+            amount_overstay++;
+        }
+        return amount_overstay;
+    }
+};
+
+
+
+ParkingLot::~ParkingLot(){
+    delete [] vehicles_parked;
+}
---
+++ ParkingLot.h
@@ -0,0 +1,28 @@
+#ifndef PARKINGLOT_H
+#define PARKINGLOT_H
+#include "Vehicle.h"
+#include "Car.h"
+#include "Bus.h"
+#include "Motorbike.h"
+using namespace std;
+
+
+class ParkingLot {
+  private:
+  int max_amount;
+  Vehicle* vehicles_parked = new Vehicle[max_amount];
+
+  public:
+  ParkingLot();
+  ParkingLot(int max_amount);
+  int getCount();
+  void parkVehicle(Vehicle* parking_space);
+  void unparkVehicle(int ID); 
+  int countOverstayingVehicles(int maxParkingDuration);
+  ~ParkingLot();
+
+  int vehicles_counter = 0;
+};
+
+
+#endif
